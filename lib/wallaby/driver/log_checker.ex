1b6f024c3dc6758fd117cb0315f2c44133a0b6ec
Tyler Young <tyler@tylerayoung.com>
2023-12-23 17:23:19 -0600

    Fix check_logs when you haven't specified performance logs

--- lib/wallaby/driver/log_checker.ex
+++ lib/wallaby/driver/log_checker.ex
@@ -6,7 +6,16 @@ defmodule Wallaby.Driver.LogChecker do
     return_value = fun.()
 
     {:ok, browser_logs} = driver.log(session, :browser)
-    {:ok, performance_logs} = driver.log(session, :performance)
+
+    performance_logs =
+      case logging_level(session, :performance) do
+        level when level in ["ALL", "DEBUG"] ->
+          {:ok, performance_logs} = driver.log(session, :performance)
+          performance_logs
+
+        _ ->
+          []
+      end
 
     session.session_url
     |> LogStore.append_logs(performance_logs ++ browser_logs)
@@ -14,4 +23,13 @@ defmodule Wallaby.Driver.LogChecker do
 
     return_value
   end
+
+  defp logging_level(%{capabilities: capabilities}, category) do
+    case capabilities do
+      %{loggingPrefs: %{^category => level}} when is_binary(level) -> level
+      _ -> nil
+    end
+  end
+
+  defp logging_level(%{parent: parent}, level), do: logging_level(parent, level)
 end
